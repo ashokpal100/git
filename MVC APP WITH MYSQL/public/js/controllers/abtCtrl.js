4e769c327560e3cc3eb6b9a18126679fd3dfceed
Ashok Pal <ashokbaghel2015@gmail.com>
2016-03-01 14:50:32 +0530

    searching with angularjs

--- MVC APP WITH MYSQL/public/js/controllers/abtCtrl.js
+++ MVC APP WITH MYSQL/public/js/controllers/abtCtrl.js
@@ -1,6 +1,16 @@
-angular.module('abtCtrl', []).controller('abtController', function($scope) 
-{
+angular.module('abtCtrl', []).controller('abtController', function($scope,$http,contactSrv) {
 
-	$scope.tagline = 'About!!!!!!!!!!';	
-	 
+    $scope.tagline = 'WEL COME TO CONTACTS LIST PAGE';
+    
+    var  refresh = function(){
+   		contactSrv.getData().success(function(response){
+	  		 console.log("i got the data i received");
+	  		 $scope.contactList= response;
+         console.log( $scope.contactList);
+	  	});
+    };
+
+   $scope.showContact = function(){
+	  	refresh();
+	};
 });
