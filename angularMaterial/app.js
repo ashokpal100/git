77768a97681f37f3a212b798c125d6c0199c731e
Ashok Pal <ashokbaghel2015@gmail.com>
2016-03-11 18:30:11 +0530

    Angular material Form

--- angularMaterial/app.js
+++ angularMaterial/app.js
@@ -1,16 +1,26 @@
 angular.module('firstApplication', ['ngMaterial']).controller('frameController', frameController);
 
-    function frameController ($scope) {  
+    function frameController ($scope) 
+    {  
             $scope.IsHidden = true;
             $scope.IsHidden1 = true;
             $scope.IsHidden2 = true;
-            $scope.ShowHide = function () {
-                $scope.IsHidden = $scope.IsHidden ? false : true;
-            }
-            $scope.ShowHide1 = function () {
-                $scope.IsHidden1 = $scope.IsHidden1 ? false : true;
-            }
-            $scope.ShowHide2 = function () {
-                $scope.IsHidden2 = $scope.IsHidden2 ? false : true;
-            } 
-    }
+            
+            $scope.items = [{"name":"Ata","email":"test@.com","number":"9090909090"},
+                            {"name":"pal","email":"test1@.com","number":"19090909090"}]
+            
+            $scope.ShowHide = function (value) {
+                   if (value=='whFrm3') 
+                    {
+                        $scope.IsHidden = $scope.IsHidden ? false : true;
+                    } 
+                    else if (value=='whFrm5')
+                    {
+                        $scope.IsHidden1 = $scope.IsHidden1 ? false : true;
+                    }
+                    else
+                    {
+                        $scope.IsHidden2 = $scope.IsHidden2 ? false : true;
+                    };
+            };
+    };
